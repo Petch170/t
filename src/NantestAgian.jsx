3f5a611b5a508f102893d0d04daf400954c91891
Nan-wp <150341587+Nan-wp@users.noreply.github.com>
2024-01-15 18:01:22 +0700

    test props componant

--- src/NantestAgian.jsx
+++ src/NantestAgian.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import App from "./App-function";
 
 export default function NantestAgian() {
   return (
     <div>
       <h1>NantestAgian</h1>
+      <App />
     </div>
   );
 }
